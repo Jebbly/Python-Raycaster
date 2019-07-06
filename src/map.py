98cfd20f577694d5aa538b3789833445004c3196
jeffliu604@gmail.com <jeffliu604@gmail.com>
2019-07-06 03:15:21 -0400

    Edited for readability

--- src/map.py
+++ src/map.py
@@ -4,8 +4,13 @@ import math
 # Import pillow to handle textures
 from PIL import Image
 
+# Define constant texture images
+BRICK = Image.open("../resources/textures/Brick/Brick-32x32.png")
+WOOD = Image.open("../resources/textures/Wood/Wood-32x32.png")
+
 class Map:
     def __init__(self):
+        # Define layout of map and height of walls
         self.wall_height = 16;
         self.layout =  [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                         [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -27,9 +32,9 @@ class Map:
                         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-        self.longest_distance = len(self.layout) * math.sqrt(2)
-        self.wall_texture = Image.open("../resources/textures/Wood/Wood-32x32.png")
-        self.split_wall_texture = self.split_pixel_columns(self.wall_texture)
+
+        # Convert selected image to columns of pixels
+        self.split_wall_texture = self.split_pixel_columns(WOOD)
 
     def split_pixel_columns(self, texture):
         # Iterate through each pixel column and add to list
